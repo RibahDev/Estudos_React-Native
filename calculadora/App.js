ef8e3594e016eb7961aa823f2519be7438fc4844
Ribeiro <joao.ribeiro17@outlook.com>
2024-05-23 22:15:05 -0300

    Finalizando parte Visual

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,10 +1,28 @@
-import { SafeAreaView, Text, StyleSheet } from "react-native";
+import { SafeAreaView, Text, StyleSheet, View } from "react-native";
 import Botao from "./src/components/Botao";
 
 export default props => {
     return(
       <SafeAreaView style={styles.container}>
-        <Botao />
+        <View style={styles.botao}>
+          <Botao label="AC" triplo />
+          <Botao label="/" orange/>
+          <Botao label="7" />
+          <Botao label="8" />
+          <Botao label="9" />
+          <Botao label="*" orange/>
+          <Botao label="4" />
+          <Botao label="5" />
+          <Botao label="6" />
+          <Botao label="-" orange/>
+          <Botao label="1" />
+          <Botao label="2" />
+          <Botao label="3" />
+          <Botao label="+" orange/>
+          <Botao label="0" duplo />
+          <Botao label="." />
+          <Botao label="=" orange/>
+        </View>
       </SafeAreaView>
     )
 }
@@ -12,9 +30,13 @@ export default props => {
 const styles = StyleSheet.create({
   container: {
     flex:1,
-    backgroundColor: "#f5fcff",
-    justifyContent: "center",
-    alignItems: "center"
+    backgroundColor: "#515151",
+    justifyContent: "flex-end"
+    
+  },
+  botao:{
+    flexDirection: "row",
+    flexWrap: "wrap"
   }
 })
 
--- calculadora/src/components/Botao.js
+++ calculadora/src/components/Botao.js
@@ -1,9 +1,18 @@
-import { Text, TouchableHighlight, View, StyleSheet } from "react-native";
+import { Text, TouchableHighlight, View, StyleSheet, Dimensions } from "react-native";
+
+// Dimensions- captura a dimensão da tela e devolve como uma constante
+const{width, height} = Dimensions.get("window");
 
 export default props => {
+    // Faz o mesmo papel de class em CSS
+    const estilosBotao = [styles.botao]
+    if(props.duplo) estilosBotao.push(styles.botaoDuplo)
+    if(props.triplo) estilosBotao.push(styles.botaoTriplo)
+    if(props.orange) estilosBotao.push(styles.botaoLaranja)
+    
     return (
         <TouchableHighlight>
-            <Text style={styles.botao}>9</Text>
+            <Text style={estilosBotao}>{props.label}</Text>
         </TouchableHighlight>
     )
 }
@@ -11,12 +20,22 @@ export default props => {
 const styles = StyleSheet.create({
     botao: {
         fontSize: 40,
-        width: 100,
-        height: 100,
+        width: width / 4,
+        height: width / 4,
         padding: 20,
         backgroundColor: "#f0f0f0",
         textAlign: "center",
-        borderWidth: 1,
+        borderWidth:1,
         borderColor: "#888"
+    },
+    botaoDuplo:{
+      width: width / 4 * 2
+    },
+    botaoTriplo:{
+      width: width / 4 * 3
+    },
+    botaoLaranja:{
+        backgroundColor: "#ff6400",
+        color: "#ffff" 
     }
 })

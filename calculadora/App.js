f4689c3386e576256f550976641a7c3c99b96195
Ribeiro <joao.ribeiro17@outlook.com>
2024-06-04 22:25:49 -0300

    Adicionando função para exibir numeros no visor

--- calculadora/App.js
+++ calculadora/App.js
@@ -1,11 +1,40 @@
 import { SafeAreaView, Text, StyleSheet, View } from "react-native";
+
 import Botao from "./src/components/Botao";
 import Visor from "./src/components/Visor";
+import { useState } from "react";
+
+const estadoInicial = {
+  valorVisor: "0",
+  limparVisor: false,
+  operacao: null,
+  valores: [0, 0],
+  posicaoCorrente: 0
+}
+
+let estadoTemp = {
+  valorVisor: "0",
+  limparVisor: false,
+  operacao: null,
+  valores: [0, 0],
+  posicaoCorrente: 0
+}
 
 export default props => {
 
+    const [estadoCalculadora, setEstadoCalculadora] = useState({...estadoInicial})
+
     adicionarDigito = n => {
-      console.warn(n)
+      if(n === '.' && estadoCalculadora.valorVisor.includes('.')){
+        return;
+      }
+
+      const limparVisor = estadoCalculadora.valorVisor === '0' || estadoCalculadora.limparVisor
+      const valorCorrente = limparVisor ? '' : estadoCalculadora.valorVisor
+      const valorVisor = valorCorrente + n
+
+      estadoTemp = {...estadoCalculadora, valorVisor, limparVisor: false}
+      setEstadoCalculadora({...estadoTemp})
     }
 
     limparMemoria = () => {
@@ -18,7 +47,7 @@ export default props => {
 
     return(
       <SafeAreaView style={styles.container}>
-        <Visor />
+        <Visor valor={estadoCalculadora.valorVisor} />
         <View style={styles.botao}>
           <Botao label="AC" triplo onClick={limparMemoria} />
           <Botao label="/" orange onClick={setOperacao} />
--- calculadora/src/components/Visor.js
+++ calculadora/src/components/Visor.js
@@ -3,7 +3,7 @@ import { View, Text, StyleSheet } from "react-native"
 export default props => {
     return (
         <View style={styles.visor}>
-            <Text style={styles.valorVisor}>10</Text>
+            <Text style={styles.valorVisor}>{props.valor}</Text>
         </View>
     )
 }

56dee3a9f2b64a3299f32b6a9139ac0a33def775
Ribeiro <joao.ribeiro17@outlook.com>
2024-07-30 20:38:28 -0300

    Adicionando passos iniciais

--- appexemploplayer/App.js
+++ appexemploplayer/App.js
@@ -1,7 +1,7 @@
 import { Text, SafeAreaView, StyleSheet } from 'react-native';
 
 // You can import supported modules from npm
-import { Card } from 'react-native-paper';
+import { Button, Card } from 'react-native-paper';
 
 // or any files within the Snack
 import AssetExample from './components/AssetExample';
@@ -9,12 +9,9 @@ import AssetExample from './components/AssetExample';
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={styles.paragraph}>
-        Change code in the editor and watch it change on your phone! Save to get a shareable url.
-      </Text>
-      <Card>
-        <AssetExample />
-      </Card>
+      <Button icon="camera" mode="contained" onPress={() => console.log("Pressed")}>
+        Press me
+      </Button>
     </SafeAreaView>
   );
 }

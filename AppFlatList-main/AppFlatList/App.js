abec87deb7a04e5d0ab6efa93a245e7baa130763
Ribeiro <joao.ribeiro17@outlook.com>
2024-08-16 21:47:59 -0300

    Adicionando Novas imagens

--- AppFlatList-main/AppFlatList/App.js
+++ AppFlatList-main/AppFlatList/App.js
@@ -5,18 +5,21 @@ import { List } from 'react-native-paper';
 const DATA = [
   {
     id: 'bd7acbea-c1b1-46c2-aed5-3ad53abb28ba',
-    titulo: 'Primeiro Item',
-    descricao: 'Descrição do Primeiro Item'
+    titulo: 'Headset Gamer',
+    descricao: 'Descrição Headset',
+    urlImage: 'https://cdn.pixabay.com/photo/2023/03/26/01/16/headphone-7877257_1280.jpg'
   },
   {
     id: '3ac68afc-c605-48d3-a4f8-fbd91aa97f63',
-    titulo: 'Segundo Item',
-    descricao: 'Descrição do Segundo Item'
+    titulo: 'Mouse Gamer',
+    descricao: 'Descrição Mouse',
+    urlImage: 'https://cdn.pixabay.com/photo/2013/07/12/17/41/computer-mouse-152249_1280.png'
   },
   {
     id: '58694a0f-3da1-471f-bd96-145571e29d72',
-    titulo: 'Terceiro Item',
-    descricao: 'Descrição do Terceiro Item'
+    titulo: 'Teclado mecânico',
+    descricao: 'Descrição Teclado',
+    urlImage: 'https://cdn.pixabay.com/photo/2022/06/11/14/22/keyboard-7256457_1280.png'
   }
 ];
 

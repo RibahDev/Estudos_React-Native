14eb505251f498f83f28a772b354fda4b4a70f8b
Ribeiro <joao.ribeiro17@outlook.com>
2024-08-23 22:14:39 -0300

    Adicionando Lista por outro componente

---
+++ AppFlatList-main/AppFlatList/components/ListaProdutoScreen.js
@@ -0,0 +1,75 @@
+
+// React
+import React from 'react';
+
+// React Native
+import {
+    SafeAreaView,
+    View,
+    FlatList,
+    StyleSheet,
+    Text,
+    StatusBar,
+    Image,
+} from 'react-native';
+
+// React Native Paper
+import {
+    List
+} from 'react-native-paper';
+
+import Produtos from '../data/Produtos'
+
+export default props => {
+
+    // Function to read title
+    const Item = ({ elemento: item }) => {
+        return (
+            <List.Item
+                title={item.titulo}
+                description={item.descricao}
+                // left={props => <List.Icon {...props} icon="folder" />}
+                left={props =>
+                    <Image {...props}
+                        style={styles.tinyLogo}
+                        source={
+                            {
+                                uri: item.urlImagem
+                            }
+                        } />}
+            />
+        )
+    };
+
+    // App
+    return (
+        <View style={styles.container}>
+            <FlatList
+                data={Produtos}
+                renderItem={({ item }) => <Item elemento={item} />}
+                keyExtractor={item => item.id}
+            />
+        </View>
+    );
+}
+
+// Style
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        marginTop: StatusBar.currentHeight || 0,
+    },
+    item: {
+        backgroundColor: '#f9c2ff',
+        padding: 20,
+        marginVertical: 8,
+        marginHorizontal: 16,
+    },
+    title: {
+        fontSize: 32,
+    },
+    tinyLogo: {
+        width: 50,
+        height: 50,
+    },
+});
